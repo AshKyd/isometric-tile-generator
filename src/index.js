77633f44bc360d9bfbdc9887e6fbea9adca2e050
Ash Kyd <ash@kyd.com.au>
2018-11-24 19:54:20 +0100

    service worker

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import App from "./components/App.vue";
 import Materialize from "materialize-css/dist/js/materialize.js";
 
 new Vue(App).$mount("#app");
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js");
+}
---
+++ src/sw.js
@@ -0,0 +1,5 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js"
+);
+
+workbox.routing.registerRoute(/.*/, workbox.strategies.staleWhileRevalidate());
